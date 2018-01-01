f89e7c15f0899bd5b44653593cbc6b7869f4bd37
avojfb <avetik.harutyunyan1@gmail.com>
2018-01-01 23:23:47 +0200

    Refactor

---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,24 @@
+// @flow
+import React from 'react';
+import Button from 'material-ui/Button';
+import AppBar from 'material-ui/AppBar';
+import Toolbar from 'material-ui/Toolbar';
+import Typography from 'material-ui/Typography';
+import './index.css';
+
+const Navbar = () => (
+  <div className="app-wrapper">
+    <AppBar position="static">
+      <div className="container">
+        <Toolbar className="toolbar">
+          <Typography type="title" color="inherit" className="app-title">
+            Shopper
+          </Typography>
+          <Button color="contrast">Login</Button>
+        </Toolbar>
+      </div>
+    </AppBar>
+  </div>
+);
+
+export default Navbar;
--- src/components/Root/index.jsx
+++ src/components/Root/index.jsx
@@ -1,24 +1,6 @@
-// @flow
 import React from 'react';
-import Button from 'material-ui/Button';
-import AppBar from 'material-ui/AppBar';
-import Toolbar from 'material-ui/Toolbar';
-import Typography from 'material-ui/Typography';
-import './index.css';
+import Navbar from '../Navbar';
 
-const Root = () => (
-  <div className="app-wrapper">
-    <AppBar position="static">
-      <div className="container">
-        <Toolbar className="toolbar">
-          <Typography type="title" color="inherit" className="app-title">
-            Shopper
-          </Typography>
-          <Button color="contrast">Login</Button>
-        </Toolbar>
-      </div>
-    </AppBar>
-  </div>
-);
+const Root = () => <Navbar />;
 
 export default Root;
