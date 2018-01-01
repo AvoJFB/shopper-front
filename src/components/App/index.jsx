255bb4531d63cb561db567e49d8f5a1992e68272
Avetik <avetik.harutyunyan1@gmail.com>
2018-01-01 20:41:04 +0200

    Initial commit

---
+++ src/components/App/index.jsx
@@ -0,0 +1,14 @@
+// @flow
+import React from 'react';
+
+const App = () => {
+  const title: string = 'Shopper';
+
+  return (
+    <div className="container">
+      {title}
+    </div>
+  );
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: './src/index.html',
+  filename: 'index.html',
+  inject: 'body',
+});
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'app.bundle.js',
+  },
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader',
+    }],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  plugins: [HtmlWebpackPluginConfig],
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    historyApiFallback: true,
+    hot: true,
+  },
+  devtool: 'source-map',
+};
